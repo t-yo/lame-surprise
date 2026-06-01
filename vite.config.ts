59e97a6b530977626a8eec8d420b1d631507f4e2
Yossra Antari <yossraantari@Mac.fritz.box>
2026-06-01 15:15:05 +0200

    renaming for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/lame-suprise-for-ella/",
+  base: "/lame-suprise/",
   plugins: [react()],
 });
